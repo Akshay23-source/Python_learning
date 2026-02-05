877080cac7275152ea8491a7bded0e9596016e78
Akshay Gabrieal R <akshaygabriealr@gmail.com>
2026-02-05 19:57:35 +0530

    Day1

---
+++ Day1.py
@@ -0,0 +1,18 @@
+print("Hello World")
+
+
+name = "Akshay"
+age = 20
+print(name)
+print(age)
+
+
+name = "Akshay"   # string
+age = 20          # integer
+height = 5.9      # float
+is_student = True # boolean
+
+
+
+name = input("Enter your name: ")
+print("Welcome", name)
