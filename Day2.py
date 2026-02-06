cb82d5ad8962435bbece73a23548669454408a18
Akshay Gabrieal R <akshaygabriealr@gmail.com>
2026-02-06 21:50:24 +0530

    Day2.py

---
+++ Day2.py
@@ -0,0 +1,29 @@
+print("Hello world")
+print(10)
+print(10+5)
+
+
+name=input("Enter your name:")
+age=input("enter your age:")
+print("Welcome",name,"your age is ",age)
+
+
+a=int(input("Enetr a number:"))
+b=int(input("eneter another number:"))
+print(a+b)
+
+
+age=int(input("Enter age:"))
+print(age+1)
+
+
+
+price=float(input("enter the price:"))
+print(price)
+
+
+p1=int(input("Enter the price:"))
+p2=int(input("enter 2nd price:"))
+p3=int(input("enter 3r price:"))
+avg=(p1+p2+p3)/3
+print(avg)
