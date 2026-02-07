4d2bb8da9c1b65fbbcfc2eeefdeeecfa0bd8b8b9
Akshay Gabrieal R <akshaygabriealr@gmail.com>
2026-02-07 12:14:24 +0530

    Day3.py

---
+++ Day3.py
@@ -0,0 +1,30 @@
+age =18
+if age>=18:
+    print("You are eligible to vote")
+    
+    
+    age =int(input("Enter your age:"))
+    if age>=18:
+        print("Eligible to vote ")
+    else:
+        print("Not eligible to vote")
+
+
+marks=int(input("Enter your marks:"))
+if marks >=90:
+    print("Grade A")
+elif marks>=75:
+    print("Grade B")
+elif marks>=50:
+    print("Grade C")
+else:
+    print("Fail")    
+   
+   
+    
+    age = 20
+citizen = True
+
+if age >= 18 and citizen:
+    print("Can vote")
+
