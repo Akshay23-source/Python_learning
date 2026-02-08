7b67cf391df17b2d7c3f2aeab2ba50e52d5b7561
Akshay Gabrieal R <akshaygabriealr@gmail.com>
2026-02-08 11:31:15 +0530

    Day4.py

---
+++ Day4.py
@@ -0,0 +1,31 @@
+for i in range (1,10):
+    print(i)
+    
+    
+    
+i=1
+while i<=5:
+    print(i)
+    i+=1    
+    
+    
+    
+    
+for i in range (1,10):
+    if i==5:
+        break
+    print(i)
+    
+    
+    
+for i in range (1,6):
+    if i ==3:
+        continue
+    print(i)
+    
+    
+    
+    
+    n=int(input("enter a number:"))
+    for i in range (1,11):
+        print(n,"x",i,"=",n*i)
