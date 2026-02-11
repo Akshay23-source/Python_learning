514f1b255d5961b305f6eff0200d7578c5e045de
Akshay Gabrieal R <akshaygabriealr@gmail.com>
2026-02-11 17:53:12 +0530

    Day7

---
+++ Day7.py
@@ -0,0 +1,51 @@
+numbers=[10,20,30,40]
+print(numbers[0])
+print(numbers[2])
+print(numbers[-1])
+
+
+
+
+numbers=[10,20,30,40,50]
+print (numbers[1:4])
+print(numbers[:3])
+
+
+
+
+numbers=[10,20,30]
+numbers[1]=99
+print(numbers)
+
+
+
+
+numbers=[10,20]
+numbers.append(30)
+numbers.insert(1,15)
+
+
+
+numbers.remove(20)
+numbers.pop()
+
+
+numbers.sort()
+numbers.reverse()
+print(len(numbers))
+
+
+
+numbers = [10, 20, 30]
+for num in numbers:
+    print(num)
+    
+    
+    
+    
+    
+    numbers = [10, 20, 30]
+print(sum(numbers))
+
+
+
