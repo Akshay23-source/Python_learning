5240dafc09b5963a5405e8fd47eece367e9ff5f8
Akshay Gabrieal R <akshaygabriealr@gmail.com>
2026-02-18 21:04:14 +0530

    Day11

---
+++ Day11.py
@@ -0,0 +1,33 @@
+@ -1,32 +0,0 @@
+class Student:
+    name = "Akshay"
+
+
+s1 = Student()
+print(s1.name)
+
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+s1 = Student("Akshay", 20)
+print(s1.name)
+print(s1.age)
+
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        print("Hello, my name is", self.name)
+
+s1 = Student("Akshay")
+s1.greet()
+
+
+
+s1 = Student("Akshay")
+s2 = Student("Rahul")
