70fb65288858f4f400cdb12d25e684771192480a
Akshay Gabrieal R <akshaygabriealr@gmail.com>
2026-02-23 20:52:50 +0530

    Day14

---
+++ Day14.py
@@ -0,0 +1,33 @@
+import math
+
+print(math.sqrt(25))
+print(math.pi)
+
+
+import math
+print(math.sqrt(16))
+
+
+
+from math import sqrt
+print(sqrt(16))
+
+
+
+import math as m
+print(m.pi)
+
+
+
+def greet(name):
+    return "Hello " + name
+
+def add(a, b):
+    return a + b
+
+
+
+import my_module
+
+print(my_module.greet("Akshay"))
+print(my_module.add(10, 20))
